0c9e1ee8bbda6fff079052b800edc5538dfed8df
Masaaki Goshima <goccy54@gmail.com>
2013-05-08 15:19:27 +0900

    work practice

--- practice/fundamentals/5th/ActionBarPractice1/src/jp/mixi/practice/actionbar/beg/MainActivity.java
+++ practice/fundamentals/5th/ActionBarPractice1/src/jp/mixi/practice/actionbar/beg/MainActivity.java
@@ -1,23 +1,55 @@
-
 package jp.mixi.practice.actionbar.beg;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.ActionBar.TabListener;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+
+public class MainActivity extends SherlockActivity implements TabListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_Sherlock);
         setContentView(R.layout.activity_main);
+        ActionBar bar = getSupportActionBar();
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        // タブを作成して追加。タブの選択・解除・再選択をハンドリングするコールバックの TabListener をセットしないと実行時例外でクラッシュする
+        bar.addTab(bar.newTab().setText("Tab1").setTabListener(this));
+        bar.addTab(bar.newTab().setText("Tab2").setTabListener(this));
+        bar.addTab(bar.newTab().setText("Tab3").setTabListener(this));
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getSupportMenuInflater().inflate(R.menu.main, menu);
+        menu.add(Menu.NONE, 0, Menu.NONE, "Login");
+        menu.add(Menu.NONE, 1, Menu.NONE, "Refresh");
         return true;
     }
 
+	@Override
+	public void onTabSelected(Tab tab,
+			FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		Toast.makeText(this, tab.getText(), Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab,
+			FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onTabReselected(Tab tab,
+			FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		//Toast.makeText(this, tab.getText(), Toast.LENGTH_LONG).show();
+	}
 }
--- practice/fundamentals/5th/InteractionCallbackPractice1/src/jp/mixi/practice/interaction/beg/MainActivity.java
+++ practice/fundamentals/5th/InteractionCallbackPractice1/src/jp/mixi/practice/interaction/beg/MainActivity.java
@@ -1,9 +1,11 @@
 
 package jp.mixi.practice.interaction.beg;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -17,7 +19,20 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        menu.add(Menu.NONE, 0, Menu.NONE, "Login");
+        menu.add(Menu.NONE, 1, Menu.NONE, "Refresh");
         return true;
     }
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // ここで、状態に応じてメニューの有効・無効を切り替えたりなどの処理をする
+        return super.onPrepareOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // 選択されたメニューに対応するイベント処理をここで実行する
+    	Toast.makeText(this, item.getTitle(),Toast.LENGTH_LONG).show();
+        return super.onOptionsItemSelected(item);
+    }
 }
